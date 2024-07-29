2fe36a17ac3d2a9b029b1a73a81a3d7fa4bc61e2
Nelbi Leon <nelbi.leon@gmail.com>
2024-07-29 17:50:03 +0200

    added libft + header file

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bineleon <neleon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 17:40:23 by bineleon          #+#    #+#             */
+/*   Updated: 2024/07/29 17:43:56 by bineleon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/ft_printf/includes/ft_printf.h"
+# include "../libft/gnl/get_next_line_bonus.h"
+# include "../libft/libft/libft.h"
+
+#endif
