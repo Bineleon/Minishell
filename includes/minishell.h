e8dc4bc5dc725ce5b84226cda7b362598d2147b8
elilliu <elilliu@student.42.fr>
2024-09-26 00:43:18 +0200

    needs freetab and utils and garbage collector

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: neleon <neleon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: elilliu <elilliu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 17:40:23 by bineleon          #+#    #+#             */
-/*   Updated: 2024/09/19 19:19:31 by neleon           ###   ########.fr       */
+/*   Updated: 2024/09/24 18:17:21 by elilliu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/wait.h>
 
 /* ╔════════════════════════════════════╗ */
 /* ║             STRUCTURES             ║ */
@@ -39,11 +43,11 @@ typedef enum e_token
 
 typedef struct s_cmd
 {
-	t_fullcmd			**str;
+	char				**str;
 	char				*cmd;
 	char				**args;
-	char				*input;
-	char				*output;
+	int					input; // a initialiser a STDIN_FILENO
+	int					output; // a initialiser a STDOUT_FILENO
 	struct s_cmd		*next;
 }						t_cmd;
 
@@ -71,10 +75,10 @@ typedef struct s_data
 	char				**envp_cpy;
 	size_t				cmds_count;
 	int					fd[2];
+	int					pid;
 	struct s_cmd		*cmds;
-	t_garbage_co *garbage; // Chained list of all the malloced pointers
+	t_garbage_co		*garbage; // Chained list of all the malloced pointers
 }						t_data;
-
 
 /* ╔════════════════════════════════════╗ */
 /* ║              PARSING               ║ */
---
+++ src/parsing/04_redirection.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   04_redirection.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elilliu <elilliu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 17:39:12 by elilliu           #+#    #+#             */
+/*   Updated: 2024/09/26 00:40:12 by elilliu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	redir_output(t_data *data, t_cmd *cmd, int is_pipe)
+{
+	int	i;
+	int	fd;
+
+	i = 0;
+	fd = 1;
+	dup2(cmd->output, STDOUT_FILENO);
+	if (is_pipe == true)
+		dup2(data->fd[1], cmd->output);
+	while (cmd->str[i])
+	{
+		if (ft_strncmp(str[i], ">") == 0)
+		{
+			if (fd && fd != 1 && fd != 2)
+				close(fd);
+			fd = open(str[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (fd == -1)
+				return ((void)printf("erreur open sortie\n"));
+		}
+		i++;
+	}
+	if (is_pipe == true)
+		dup2(fd, data->fd[1]);
+	else if (fd != 1)
+		dup2(fd, cmd->output);
+}
+
+void	redir_input(t_data *data, t_cmd *cmd)
+{
+	int	i;
+	int	fd;
+
+	i = 0;
+	fd = 0;
+	dup2(cmd->input, STDIN_FILENO);
+	while (cmd->str[i])
+	{
+		if (ft_strncmp(str[i], "<") == 0)
+		{
+			if (fd && fd != 1 && fd != 2)
+				close(fd);
+			fd = open(str[i + 1], O_RDONLY);
+			if (fd == -1)
+				return ((void)printf("erreur open\n"));
+		}
+		i++;
+	}
+	if (fd)
+		dup2(fd, cmd->input);
+}
+
+char	**jointab(char **tab, char *str)
+{
+	int		i;
+	char	**newtab;
+
+	i = 0;
+	while (tab[i])
+		i++;
+	newtab = malloc(sizeof(char *) * (i + 1));
+	if (!newtab)
+		return (NULL);
+	newtab[0] = ft_strdup(str);
+	if (!newtab[0])
+		return (free(newtab), NULL);
+	i = 0;
+	while (tab[i])
+	{
+		newtab[i + 1] = ft_strdup(tab[i]);
+		if (!newtab[i + 1])
+			return (freetab(newtab), NULL);
+		i++;
+	}
+	return (newtab);
+}
+
+void	exec_cmd(t_data *data, t_cmd *cmd, int is_pipe)
+{
+	redir_input(data, cmd);
+	redir_output(data, cmd, is_pipe);
+	execute(data, cmd);
+}
+
+int	redirection(t_data *data)
+{
+	while (data->cmds->next != NULL)
+	{
+		if (pipe(data->fd) == -1)
+			return (printf("message d'erreur a inserer\n"), 0);
+		data->pid = fork();
+		if (data->pid == -1)
+			return (printf ("message d'erreur fork\n"), 0);
+		if (data->pid == 0)
+			exec_cmd(data, data->cmds, true);
+		dup2(data->fd[0], data->cmds->next->input);
+		close(data->fd[0]);
+		close(data->fd[1]);
+		data->cmds = data->cmds->next;
+	}
+}
+
