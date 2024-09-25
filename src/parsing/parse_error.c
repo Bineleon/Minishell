0b72796dd56b8ca1c1ecc4ea430cbcb909ad9b70
Nelbi Leon <neleon@student.42.fr>
2024-09-25 23:51:48 +0200

    ONGOING : check_errors

--- src/parsing/parse_error.c
+++ src/parsing/parse_error.c
@@ -3,11 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   parse_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bineleon <neleon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: neleon <neleon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 17:06:38 by neleon            #+#    #+#             */
-/*   Updated: 2024/09/25 19:07:55 by bineleon         ###   ########.fr       */
+/*   Updated: 2024/09/25 23:51:08 by neleon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+t_bool	check_errors(char *line)
+{
+}
