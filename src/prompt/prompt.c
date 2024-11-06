34cc2d61db347b15c7cf81af4c0dc143159e58f6
Nelbi Leon <nelbi.leon@gmail.com>
2024-11-06 15:07:36 +0100

    ONGOING : env

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -49,10 +49,6 @@ void	ft_prompt(t_data *data)
 	while (1)
 	{
 		printf(CYAN);
-		if (isatty(STDIN_FILENO) == 1)
-			printf("Pipi\n");
-		else
-			printf("caca\n");
 		prompt = readline("Minishell>\033[0m ");
 		if (!prompt)
 		{
@@ -67,13 +63,9 @@ void	ft_prompt(t_data *data)
 		if (prompt && *prompt)
 		{
 			add_history(prompt);
-			printf("1\n");
 			check_open_quotes(prompt);
-			printf("2\n");
 			tokens = parse_tokens(prompt, data);
-			printf("3\n");
 			check_errors(tokens);
-			printf("4\n");
 			// if (!check_errors(tokens))
 			// {
 			// 	printf("1st tokenization : \n");
@@ -81,7 +73,6 @@ void	ft_prompt(t_data *data)
 			// }
 			// printf("post expand : \n");
 			expand_var(data);
-			printf("5\n");
 			// print_tokens(tokens);
 			// if (ft_strncmp("pwd", data->token_fullcmd->str, ft_strlen(data->token_fullcmd->str)) == 0)
 			// 	ft_pwd();
